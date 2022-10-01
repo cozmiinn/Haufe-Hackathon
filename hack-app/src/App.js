ed2aa8d44331162b4d0965fb0c6ce411bad5a8b7
cozmiinn <cosmiin.mihalesull@gmail.com>
2022-10-01 12:53:50 +0300

    implementated the basic maps

--- hack-app/src/App.js
+++ hack-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import Map from './components/Map';
 import './App.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 // TODO: start here
 
@@ -8,7 +9,17 @@ function App() {
   return (
     <>
       <h1>Haufe Hackathon</h1>
-      {/* <Map/> */}
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.10]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </>
   );
 }
