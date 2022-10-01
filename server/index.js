cb2ee4f2422c4f613134ed6c8c9d89ddc6763320
cozmiinn <cosmiin.mihalesull@gmail.com>
2022-10-01 14:40:01 +0300

    Started listening on portr 4000

--- server/index.js
+++ server/index.js
@@ -1,1 +1,23 @@
 // TODO: start here
+//express exemple
+
+
+const app = require("express")();
+const axios = require("axios");
+const cors = require("cors");
+
+const port = 4000;
+
+app.use(cors());
+
+app.get("/", (req, resp) => {
+    axios.get("https://data.primariatm.ro/api/3/action/datastore_search").then(response => {
+        resp.status(200).json(response);
+    });
+
+});
+
+
+app.listen(port, () => {
+    console.log("Started listening on port 4000");
+});
